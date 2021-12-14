49fb57fa84815dd8a564c852f0909d257858e529
Kaitlyn Johnson <k.john@utexas.edu>
2021-12-14 14:50:46 -0500

    removed duplicate line

--- scripts/make_flourish_dataframe_omicron.R
+++ scripts/make_flourish_dataframe_omicron.R
@@ -521,8 +521,7 @@ cases_recent<-global_t%>%filter(submission_date==LAST_DATA_PULL_DATE)%>%
   mutate(change_cases_rolling_7_day_sum = (rolling_cases_last_7_days - rolling_cases_lag_7),
          pctchange_cases_rolling_7_day_sum = round(100*change_cases_rolling_7_day_sum/(rolling_cases_lag_7),1))%>%
   select(!submission_date)
-type = "global_cases"
-cases_recent<-rbind(type, cases_recent)
+
 # rename so we can bind to toplin df
 cases_df<-cases_recent%>%select(!rolling_cases_lag_7)%>%rename(
   n = rolling_cases_last_7_days, change = change_cases_rolling_7_day_sum,
