de1429218250247514c4de47f756ccc10f8c38bf
Kaitlyn Johnson <kjohnson@rockfound.org>
2021-12-23 11:42:48 -0500

    read in all omicron datasets in a folder

--- scripts/summarize_metadata_to_country_day.R
+++ scripts/summarize_metadata_to_country_day.R
@@ -25,27 +25,11 @@ today <- chartr(old = '-', new = '_', today)
 today <- chartr(old = ' ', new = '_', today)
 #today<-"2021_12_22_13"
 
-# Domino path
-path1 <- paste0('/mnt/data/raw/gisaid_hcov-19_2021_12_20_13.tsv')# data before 12/17
-path2 <- paste0('/mnt/data/raw/gisaid_hcov-19_2021_12_22_12.tsv')# data from 12/18-12/20
-path3 <- paste0('/mnt/data/raw/gisaid_hcov-19_2021_12_23_12.tsv') # data from 12/21-12/22
-path4 <- paste0('/mnt/data/raw/gisaid_hcov-19_', today, '.tsv')
-# local path
-#path <- paste0('../data/raw/gisaid_hcov-19_', today, '.tsv')
- 
-# Manual binding of GISAID sequences (max single download is 10,000)
-x1 <- read_delim(path, delim = '\t',col_types = 'ccDc' ) %>% 
-  janitor::clean_names()
-x2 <- read_delim(path2, delim = '\t', col_types = 'ccDc') %>% 
-  janitor::clean_names()
-x3 <- read_delim(path3, delim = '\t', col_types = 'ccDc') %>% 
-  janitor::clean_names()
-x4 <- read_delim(path4, delim = '\t', col_types = 'ccDc') %>% 
+x <- paste0('/mnt/data/raw/gisaid_omicron_seq/', dir(path='/mnt/data/raw/gisaid_omicron_seq')) %>%
+  map(read_delim, delim = '\t', col_types = 'ccDc') %>% 
+  bind_rows() %>% 
   janitor::clean_names()
 
-x<-rbind(x1, x2)
-x<-rbind(x,x3)
-x<-rbind(x,x4)
 
 
 # Process data ------------------------------------------------------------
