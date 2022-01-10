d5dff070758386b8aeca664b7def40ce7b20576e
Kaitlyn Johnson <kjohnson@rockfound.org>
2022-01-10 14:36:16 -0500

    fixed USE_CASE

--- scripts/gisaid_metadata_processing.R
+++ scripts/gisaid_metadata_processing.R
@@ -9,7 +9,7 @@ rm(list = ls())
 rm(list = ls())
 global_var = Sys.getenv("USE_CASE")
 if(global_var == ""){
-  USE_CASE=='local'
+  USE_CASE<-'local'
 }
 #USE_CASE = 'domino' # 'domino' or 'local'
 
